23d59c7430f770f1ac5f6d1f8ef4d090a701d808
Efrain <efrondx@gmail.com>
2024-11-20 10:58:05 -0600

    Fix lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-{/*import Article from './components/Article';*/}
 import Elements from './components/Elements';
 import Scripts from './components/Scripts';
 import Delocalizers from './components/Delocalizers';
